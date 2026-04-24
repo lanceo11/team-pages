93d678b732de2d6efba9afb4a1bb853a52df41a3
ArjunG1031 <arjun.redranger@gmail.com>
2026-04-23 23:04:16 -0700

    Sync GameLevelSeek source imports with published runner

--- _projects/characters/levels/GameLevelSeek.js
+++ _projects/characters/levels/GameLevelSeek.js
@@ -1,8 +1,8 @@
-import GameEnvBackground from '../../../assets/js/GameEnginev1.1/essentials/GameEnvBackground.js';
-import Player from '../../../assets/js/GameEnginev1.1/essentials/Player.js';
-import Npc from '../../../assets/js/GameEnginev1.1/essentials/Npc.js';
-import Barrier from '../../../assets/js/GameEnginev1.1/essentials/Barrier.js';
-import Collectible from '../../../assets/js/GameEnginev1.1/essentials/Collectible.js';
+import GameEnvBackground from '../../../GameEnginev1.1/essentials/GameEnvBackground.js';
+import Player from '../../../GameEnginev1.1/essentials/Player.js';
+import Npc from '../../../GameEnginev1.1/essentials/Npc.js';
+import Barrier from '../../../GameEnginev1.1/essentials/Barrier.js';
+import Collectible from '../../../GameEnginev1.1/essentials/Collectible.js';
 
 console.log('GameLevelSeek.js loaded:', new Date().toISOString());
 
